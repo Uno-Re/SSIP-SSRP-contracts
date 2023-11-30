f3a7fc2088b8e640bc6afab8587175c70622aa16
wankhede04 <wankhedevijay04@gmail.com>
2023-11-30 19:44:59 +0530

    feat: add pool killed check in harvest in SyntheticSSIP

--- contracts/SyntheticSSIP.sol
+++ contracts/SyntheticSSIP.sol
@@ -204,7 +204,7 @@ contract SyntheticSSIP is ISyntheticSSIP, ReentrancyGuard, Ownable, Pausable {
         emit LogLeaveFromPending(msg.sender, address(this), pendingWR);
     }
 
-    function harvest(address _to) external override whenNotPaused nonReentrant {
+    function harvest(address _to) external override whenNotPaused isPoolNotKilled nonReentrant {
         _harvest(_to);
     }
 
