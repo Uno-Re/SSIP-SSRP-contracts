ca76bc763432eaaab8eef086d8f0cd509eb9b13f
wankhede04 <wankhedevijay04@gmail.com>
2023-12-07 12:33:13 +0530

    test: add test for claim policy in SSIP

---
+++ test/claim.test.js
@@ -0,0 +1,205 @@
+const { expect } = require("chai")
+const { ethers, network, upgrades } = require("hardhat")
+const { getBigNumber, getNumber, advanceBlock, advanceBlockTo } = require("../scripts/shared/utilities")
+const { BigNumber } = ethers
+const UniswapV2Router = require("../scripts/abis/UniswapV2Router.json")
+const SalesPolicy = require("../scripts/abis/SalesPolicy.json")
+const {
+  WETH_ADDRESS,
+  UNISWAP_FACTORY_ADDRESS,
+  UNISWAP_ROUTER_ADDRESS,
+  TWAP_ORACLE_PRICE_FEED_FACTORY,
+  UNO,
+  USDT,
+  UNO_USDT_PRICE_FEED,
+} = require("../scripts/shared/constants")
+const { clearConfigCache } = require("prettier")
+
+describe.only("ssssss", function () {
+  before(async function () {
+    this.ExchangeAgent = await ethers.getContractFactory("ExchangeAgent")
+    this.SingleSidedInsurancePool = await ethers.getContractFactory("SingleSidedInsurancePool")
+    this.CapitalAgent = await ethers.getContractFactory("CapitalAgent")
+    this.RiskPoolFactory = await ethers.getContractFactory("RiskPoolFactory")
+    this.RiskPool = await ethers.getContractFactory("RiskPool")
+    this.Rewarder = await ethers.getContractFactory("Rewarder")
+    this.RewarderFactory = await ethers.getContractFactory("RewarderFactory")
+    this.SyntheticSSIPFactory = await ethers.getContractFactory("SyntheticSSIPFactory")
+    this.MockUNO = await ethers.getContractFactory("MockUNO")
+    this.MockUSDT = await ethers.getContractFactory("MockUSDT")
+    this.RewardAttack = await ethers.getContractFactory("RewardAttack")
+    this.OptimisticOracleV3 = await ethers.getContractFactory("OptimisticOracleV3")
+    this.SalesPolicy = await ethers.getContractFactory("MockSalesPolicy")
+    this.SalesPolicyFactory = await ethers.getContractFactory("MockSalesPolicyFactory")
+    this.PremiumPool = await ethers.getContractFactory("PremiumPool")
+    this.signers = await ethers.getSigners()
+    this.zeroAddress = "0x0000000000000000000000000000000000000000";
+
+    this.routerContract = new ethers.Contract(
+      UNISWAP_ROUTER_ADDRESS.rinkeby,
+      JSON.stringify(UniswapV2Router.abi),
+      ethers.provider,
+    )
+  })
+
+  beforeEach(async function () {
+    this.mockUNO = await this.MockUNO.deploy()
+    this.mockUSDT = await this.MockUSDT.deploy()
+    await this.mockUNO.connect(this.signers[0]).faucetToken(getBigNumber("500000000"), { from: this.signers[0].address })
+    await this.mockUSDT.connect(this.signers[0]).faucetToken(getBigNumber("500000"), { from: this.signers[0].address })
+    await this.mockUNO.connect(this.signers[1]).faucetToken(getBigNumber("500000000"), { from: this.signers[1].address })
+    await this.mockUSDT.connect(this.signers[1]).faucetToken(getBigNumber("500000"), { from: this.signers[1].address })
+    await this.mockUNO.connect(this.signers[2]).faucetToken(getBigNumber("500000000"), { from: this.signers[2].address })
+    await this.mockUSDT.connect(this.signers[2]).faucetToken(getBigNumber("500000"), { from: this.signers[2].address })
+    this.masterChefOwner = this.signers[0].address
+    this.claimAssessor = this.signers[3].address
+    this.riskPoolFactory = await this.RiskPoolFactory.deploy()
+    this.rewarderFactory = await this.RewarderFactory.deploy()
+    this.syntheticSSIPFactory = await this.SyntheticSSIPFactory.deploy()
+
+    const assetArray = [this.mockUSDT.address, this.mockUNO.address, this.zeroAddress]
+
+    const timestamp = new Date().getTime()
+
+    await (
+      await this.mockUNO
+        .connect(this.signers[0])
+        .approve(UNISWAP_ROUTER_ADDRESS.rinkeby, getBigNumber("10000000"), { from: this.signers[0].address })
+    ).wait()
+    await (
+      await this.mockUSDT
+        .connect(this.signers[0])
+        .approve(UNISWAP_ROUTER_ADDRESS.rinkeby, getBigNumber("10000000"), { from: this.signers[0].address })
+    ).wait()
+
+    console.log("Adding liquidity...")
+
+    await (
+      await this.routerContract
+        .connect(this.signers[0])
+        .addLiquidity(
+          this.mockUNO.target,
+          this.mockUSDT.target,
+          getBigNumber("3000000"),
+          getBigNumber("3000", 6),
+          getBigNumber("3000000"),
+          getBigNumber("3000", 6),
+          this.signers[0].address,
+          timestamp,
+          { from: this.signers[0].address, gasLimit: 9999999 },
+        )
+    ).wait()
+
+    this.exchangeAgent = await this.ExchangeAgent.deploy(
+      this.mockUSDT.target,
+      WETH_ADDRESS.rinkeby,
+      TWAP_ORACLE_PRICE_FEED_FACTORY.rinkeby,
+      UNISWAP_ROUTER_ADDRESS.rinkeby,
+      UNISWAP_FACTORY_ADDRESS.rinkeby,
+      this.signers[0].address,
+    )
+
+    this.capitalAgent = await upgrades.deployProxy(
+      this.CapitalAgent, [
+        this.exchangeAgent.target, 
+        this.mockUNO.target,
+        this.mockUSDT.target,
+        this.signers[0].address,
+        this.signers[0].address]
+    );
+
+    this.premiumPool = await this.PremiumPool.deploy(
+      this.exchangeAgent.target,
+      this.mockUNO.target,
+      this.mockUSDT.target,
+      this.signers[0].address,
+    )
+
+    this.optimisticOracleV3 = await this.OptimisticOracleV3.deploy();
+    this.singleSidedInsurancePool = await upgrades.deployProxy(
+      this.SingleSidedInsurancePool, [
+        this.capitalAgent.target,
+        this.signers[0].address,
+        this.signers[0].address,
+        this.signers[0].address
+      ],
+      {
+        unsafeAllow: ["constructor", "state-variable-immutable"],
+        constructorArgs: [this.mockUNO.target, this.optimisticOracleV3.target],
+      },
+    );
+
+    await (await this.capitalAgent.addPoolWhiteList(this.singleSidedInsurancePool.target)).wait()
+    await this.singleSidedInsurancePool.createRewarder(
+      this.signers[0].address,
+      this.rewarderFactory.target,
+      this.mockUNO.target,
+    )
+    this.rewardertarget = await this.singleSidedInsurancePool.rewarder()
+    this.rewarder = await this.Rewarder.attach(this.rewardertarget)
+
+    expect(this.rewarder.target).equal(await this.singleSidedInsurancePool.rewarder())
+
+    await (await this.mockUNO.transfer(this.rewarder.target, getBigNumber("100000"))).wait()
+
+    this.rewardAttack = await this.RewardAttack.deploy()
+
+    this.salesPolicyFactory = await this.SalesPolicyFactory.deploy(this.mockUSDT.target, this.mockUSDT.target,this.mockUSDT.target, this.capitalAgent.target, this.signers[0].address);
+  })
+
+  describe("SingleSidedInsurancePool Actions", function () {
+    beforeEach(async function () {
+      console.log("[action start ========>]")
+      await this.singleSidedInsurancePool.createRiskPool(
+        "UNO-LP",
+        "UNO-LP",
+        this.riskPoolFactory.target,
+        this.mockUSDT.target,
+        getBigNumber("1"),
+        getBigNumber("10", 6),
+      )
+      await this.mockUSDT.approve(this.singleSidedInsurancePool.target, getBigNumber("1000000"))
+      await this.mockUSDT
+        .connect(this.signers[1])
+        .approve(this.singleSidedInsurancePool.target, getBigNumber("1000000"), { from: this.signers[1].address })
+      await this.mockUNO
+        .connect(this.signers[2])
+        .approve(this.singleSidedInsurancePool.target, getBigNumber("1000000"), { from: this.signers[2].address })
+      await this.mockUSDT
+        .connect(this.signers[2])
+        .approve(this.singleSidedInsurancePool.target, getBigNumber("1000000"), { from: this.signers[2].address })
+  
+      const poolInfo = await this.singleSidedInsurancePool.poolInfo()
+      expect(poolInfo.unoMultiplierPerBlock).equal(getBigNumber("1"))
+      this.poolAddress = await this.singleSidedInsurancePool.riskPool()
+      await this.singleSidedInsurancePool.connect(this.signers[2]).enterInPool(getBigNumber("8500"), {from: this.signers[2].address})
+    })
+
+    describe("SingleSidedInsurancePool Claim", function () {
+
+      it.only("Should claim by claimAssessor and then check LP token worth", async function () {
+        // await this.capitalAgent.addPoolByAdmin(this.singleSidedInsurancePool.target, this.mockUNO.target, 0);
+        await this.capitalAgent.setMLR(getBigNumber("10"));
+        await this.singleSidedInsurancePool.enterInPool(getBigNumber("10000"))
+        const riskPool = this.RiskPool.attach(this.poolAddress)
+        await this.capitalAgent.setSalesPolicyFactory(this.salesPolicyFactory.target);
+        let salesPolicy = await this.salesPolicyFactory.newSalesPolicy(this.exchangeAgent.target, this.mockUNO.target, this.capitalAgent.target);
+        let a = await this.capitalAgent.getPolicyInfo();
+        await this.singleSidedInsurancePool.enterInPool(getBigNumber("1000"))
+        this.salesPolicy = await ethers.getContractAt("MockSalesPolicy", a[0]);
+        await this.salesPolicy.buyPol(1000, this.mockUNO.target, getBigNumber("100"));
+        const lpPriceBefore = await riskPool.lpPriceUno()
+        expect(lpPriceBefore).to.equal(getBigNumber("1"))
+        let b = await this.salesPolicy.getPolicyData(1000);
+        await this.singleSidedInsurancePool.setClaimAssessor(this.signers[0].address);
+        console.log(await this.singleSidedInsurancePool.hasRole("0x8114cb781eb72f8b61c388eb9caa27d853c88b65c67c47dd0f0207ba916190d6", this.signers[0].address));
+        await this.singleSidedInsurancePool
+          .requestPayout(1000, this.signers[5].address, getBigNumber("100"))
+
+        await this.optimisticOracleV3.settle("0x6100000000000000000000000000000000000000000000000000000000000000", this.singleSidedInsurancePool.target);
+        console.log(await this.mockUSDT.balanceOf(this.signers[5].address));
+        expect(await this.mockUSDT.balanceOf(this.signers[5].address)).to.equal(getBigNumber("100"));
+      })
+    })
+  })
+})
