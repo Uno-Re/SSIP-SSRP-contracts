0c98b09ff0b7f95992690673fb1e927759e544eb
wankhede04 <wankhedevijay04@gmail.com>
2023-12-12 22:16:52 +0530

    update deploy script of CapitalAgent

--- deploy/3_CapitalAgent.js
+++ deploy/3_CapitalAgent.js
@@ -10,7 +10,8 @@ module.exports = async function ({ ethers, getNamedAccounts, deployments, getCha
   // const multiSigWallet = await deployments.get("MultiSigWallet")
   const UNO = "0x474021845c4643113458ea4414bdb7fb74a01a77"
   const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
-  const exchangeAgent = "0x6aC1081CBb92524170E61CFFD37bDaF3b38FBC4c"
+  // const exchangeAgent = "0x6aC1081CBb92524170E61CFFD37bDaF3b38FBC4c"
+  const exchangeAgent = await hre.deployments.get("ExchangeAgent")
   const multiSigWallet = "0x8c3d5c9538256DAB8Eb4B197370574340fe3254F"
   const operator = "0x721d214267247568DA3A9123abfAc71fc18a5EE4"
 
