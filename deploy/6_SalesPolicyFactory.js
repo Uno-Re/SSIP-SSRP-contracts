9e208aab6152eae1d2391a63e9b3f2d1aa495d27
wankhede04 <wankhedevijay04@gmail.com>
2023-12-12 22:20:07 +0530

    update deploy script of SalesPolicyFactory

--- deploy/6_SalesPolicyFactory.js
+++ deploy/6_SalesPolicyFactory.js
@@ -1,5 +1,6 @@
 // Defining bytecode and abi from original contract on mainnet to ensure bytecode matches and it produces the same pair code hash
 
+const hre = require("hardhat");
 module.exports = async function ({ ethers, getNamedAccounts, deployments, getChainId }) {
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
@@ -14,10 +15,13 @@ module.exports = async function ({ ethers, getNamedAccounts, deployments, getCha
   // const multiSigWallet = await deployments.get("MultiSigWallet")
 
   const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
-  const exchangeAgent = "0x6aC1081CBb92524170E61CFFD37bDaF3b38FBC4c"
-  const premiumPool = "0xdB4B701f1a4653BFD5F0f4EFF1913aEAF5E21E68"
-  const capitalAgent = "0x0bCed28f17a0c8CB66c07dD1a4ccfb2ef3159c05"
+  // const exchangeAgent = "0x6aC1081CBb92524170E61CFFD37bDaF3b38FBC4c"
+  const exchangeAgent = await hre.deployments.get("ExchangeAgent")
+  // const premiumPool = "0xdB4B701f1a4653BFD5F0f4EFF1913aEAF5E21E68"
+  const premiumPool = await hre.deployments.get("PremiumPool")
+  // const capitalAgent = "0x0bCed28f17a0c8CB66c07dD1a4ccfb2ef3159c05"
   const multiSigWallet = "0x8c3d5c9538256DAB8Eb4B197370574340fe3254F"
+  const capitalAgent = await hre.deployments.get("CapitalAgent")
 
   await deploy("SalesPolicyFactory", {
     from: deployer,
