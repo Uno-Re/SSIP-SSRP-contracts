1985c57696805e8e35d17af0c49fac9c4b7ffafd
wankhede04 <wankhedevijay04@gmail.com>
2023-12-12 22:16:37 +0530

    update deploy script of PremiumPool

--- deploy/2_PremiumPool.js
+++ deploy/2_PremiumPool.js
@@ -11,12 +11,14 @@ module.exports = async function ({ ethers, getNamedAccounts, deployments, getCha
 
   const UNO = "0x474021845c4643113458ea4414bdb7fb74a01a77"
   const USDC = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
-  const exchangeAgent = "0x6aC1081CBb92524170E61CFFD37bDaF3b38FBC4c"
+  // const exchangeAgent = "0x6aC1081CBb92524170E61CFFD37bDaF3b38FBC4c"
+  const exchangeAgent = await hre.deployments.get("ExchangeAgent")
   const multiSigWallet = "0x8c3d5c9538256DAB8Eb4B197370574340fe3254F"
+  const governance = ""
 
   const premiumPool = await deploy("PremiumPool", {
     from: deployer,
-    args: [exchangeAgent, UNO, USDC, multiSigWallet],
+    args: [exchangeAgent, UNO, USDC, multiSigWallet, governance],
     log: true,
     deterministicDeployment: false,
   })
