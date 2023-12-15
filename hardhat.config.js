08ff0cbbb58a6b035e195fba5278a664f78f61cd
wankhede04 <wankhedevijay04@gmail.com>
2023-12-15 20:28:59 +0530

    remove comment code

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,7 +25,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // Go to https://hardhat.org/config/ to learn more
 
 const accounts = {
-  // mnemonic: process.env.MNEMONIC || "test test test test test test test test test test test junk",
+  mnemonic: process.env.MNEMONIC || "test test test test test test test test test test test junk",
   // accountsBalance: "990000000000000000000",
 }
 
@@ -35,7 +35,7 @@ const accounts = {
 module.exports = {
   defaultNetwork: 'hardhat',
   gasReporter: {
-    // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     currency: "USD",
     enabled: true
   },
@@ -47,52 +47,52 @@ module.exports = {
       default: 1,
     },
   },
-  // networks: {
-  //   hardhat: {
-  //     hardfork: "london",
-  //     allowUnlimitedContractSize: true,
-  //     settings: {
-  //       optimizer: {
-  //         enabled: true,
-  //         runs: 9999,
-  //       },
-  //     },
-  //     evmVersion: "byzantium",
-  //     forking: {
-  //       url: 'https://eth-rinkeby.alchemyapi.io/v2/8SAQa7xMc0VXTR_hyfPvAt2pe3QrXybB',
-  //       // url: 'https://eth-mainnet.alchemyapi.io/v2/kX2m_40xGyLvewVGbo7JaAe6mZTha838',
-  //       enabled: true,
-  //       // blockNumber: 7041459 //6430278 //7041458 //6615559 10207859 11869355        
-  //     },
-  //     gasPrice: "auto",
-  //     accounts
-  //   },
-  //   mainnet: {
-  //     url: `https://mainnet.infura.io/v3/${process.env.MAINNET_INFURA_KEY}`,
-  //     accounts,
-  //     chainId: 1,
-  //     live: false,
-  //     saveDeployments: true
-  //   },
-  //   bscTest: {
-  //     url: "https://data-seed-prebsc-1-s1.binance.org:8545",
-  //     chainId: 97,
-  //     accounts: {mnemonic: process.env.MNEMONIC},
-  //     live: true,
-  //     saveDeployments: true,
-  //     gasMultiplier: 2,
-  //   },
-  //   bscMain: {
-  //     url: "https://bsc-dataseed.binance.org/",
-  //     chainId: 56,
-  //     accounts: {mnemonic: process.env.MNEMONIC},
-  //     live: true,
-  //     saveDeployments: true
-  //   }
-  // },
-  // etherscan: {
-  //   apiKey: process.env.ETHERSCAN_API_KEY // BSC_API_KEY
-  // },
+  networks: {
+    hardhat: {
+      hardfork: "london",
+      allowUnlimitedContractSize: true,
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 9999,
+        },
+      },
+      evmVersion: "byzantium",
+      forking: {
+        url: 'https://eth-rinkeby.alchemyapi.io/v2/8SAQa7xMc0VXTR_hyfPvAt2pe3QrXybB',
+        // url: 'https://eth-mainnet.alchemyapi.io/v2/kX2m_40xGyLvewVGbo7JaAe6mZTha838',
+        enabled: true,
+        // blockNumber: 7041459 //6430278 //7041458 //6615559 10207859 11869355        
+      },
+      gasPrice: "auto",
+      accounts
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.MAINNET_INFURA_KEY}`,
+      accounts,
+      chainId: 1,
+      live: false,
+      saveDeployments: true
+    },
+    bscTest: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      accounts: {mnemonic: process.env.MNEMONIC},
+      live: true,
+      saveDeployments: true,
+      gasMultiplier: 2,
+    },
+    bscMain: {
+      url: "https://bsc-dataseed.binance.org/",
+      chainId: 56,
+      accounts: {mnemonic: process.env.MNEMONIC},
+      live: true,
+      saveDeployments: true
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY // BSC_API_KEY
+  },
   paths: {
     deploy: "deploy",
     deployments: "deployments",
