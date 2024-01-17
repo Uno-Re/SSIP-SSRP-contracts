bc964d207bdca757d1209fdf840e1a37015a9f27
wankhede04 <wankhedevijay04@gmail.com>
2024-01-18 02:47:03 +0530

    update variable to dotenv in deploy script

--- deploy/04_PremiumPool.js
+++ deploy/04_PremiumPool.js
@@ -1,16 +1,17 @@
 // Defining bytecode and abi from original contract on mainnet to ensure bytecode matches and it produces the same pair code hash
 
 const hre = require("hardhat");
+require("dotenv").config();
+
 module.exports = async function ({ ethers, getNamedAccounts, deployments, getChainId }) {
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
 
   const mockUSDT = await hre.deployments.get("MockUSDT")
   const mockUNO = await hre.deployments.get("MockUNO")
-  // const exchangeAgent = "0x6aC1081CBb92524170E61CFFD37bDaF3b38FBC4c"
   const exchangeAgent = await hre.deployments.get("ExchangeAgent")
-  const multiSigWallet = "0xedFFe0a06914c9D6083B4B099e5b935E9E84c9a5"
-  const governance = "0xedFFe0a06914c9D6083B4B099e5b935E9E84c9a5"
+  const multiSigWallet = process.env.MULTISIGWALLET;
+  const governance = process.env.GOVERNANCE;
 
   const premiumPool = await deploy("PremiumPool", {
     from: deployer,
--- deploy/09_EscalationManager.js
+++ deploy/09_EscalationManager.js
@@ -1,11 +1,12 @@
 // Defining bytecode and abi from original contract on mainnet to ensure bytecode matches and it produces the same pair code hash
+require("dotenv").config()
 
 module.exports = async function ({ ethers, getNamedAccounts, deployments, getChainId }) {
     const { deploy } = deployments
     const { deployer } = await getNamedAccounts()
 
-    const optimisticOracleV3 = "0x9923D42eF695B5dd9911D05Ac944d4cAca3c4EAB"
-    const governance = "0xedFFe0a06914c9D6083B4B099e5b935E9E84c9a5"
+    const optimisticOracleV3 = process.env.OPTIMISTIC_ORACLE_V3;
+    const governance = process.env.GOVERNANCE;
   
     const escalationManager = await deploy("EscalationManager", {
       from: deployer,
