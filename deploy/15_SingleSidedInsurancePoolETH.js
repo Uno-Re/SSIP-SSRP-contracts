f942f7558b8f29058aa1d99a6cfc8833d00f7f80
wankhede04 <wankhedevijay04@gmail.com>
2024-01-24 18:28:04 +0530

    fix: constructor param gaurdianCouncil -> claimsDAO

--- deploy/15_SingleSidedInsurancePoolETH.js
+++ deploy/15_SingleSidedInsurancePoolETH.js
@@ -16,6 +16,7 @@ module.exports = async function ({getNamedAccounts, deployments}) {
   const guardianCouncil = process.env.GAURDIAN_COUNCIL;
   const defaultCurrency = process.env.DEAFAULT_CURRENCY;
   const optimisticOracleV3 = process.env.OPTIMISTIC_ORACLE_V3;
+  const claimsDao = process.env.CLAIMS_DAO;
 
   const ssip = await deploy("SingleSidedInsurancePoolETH", {
     from: deployer,
@@ -46,7 +47,7 @@ module.exports = async function ({getNamedAccounts, deployments}) {
       execute: {
         init: {
           methodName: "initialize",
-          args: [ssip.address, optimisticOracleV3, defaultCurrency, claimProcessor.address, escalationManager.address, guardianCouncil],
+          args: [ssip.address, optimisticOracleV3, defaultCurrency, escalationManager.address, guardianCouncil, claimsDao],
         },
       },
       proxyContract: "OpenZeppelinTransparentProxy",
