07c97bffc715ec9ce583553126d86dd7d5975184
wankhede04 <wankhedevijay04@gmail.com>
2024-02-20 13:01:08 +0530

    feat: add admin role check in setUserDetials

--- contracts/SingleSidedInsurancePoolBSC.sol
+++ contracts/SingleSidedInsurancePoolBSC.sol
@@ -8,7 +8,7 @@ contract SingleSidedInsurancePoolBSC is SingleSidedInsurancePool {
 
     event LogUserUpdated(address indexed pool, address indexed user, uint256 amount);
 
-    function setUserDetails(address _user, uint256 _amount, uint256 _rewardDebt) external {
+    function setUserDetails(address _user, uint256 _amount, uint256 _rewardDebt) external onlyRole(ADMIN_ROLE) roleLockTimePassed(ADMIN_ROLE) {
         userInfo[_user].amount = _amount;
         userInfo[_user].rewardDebt = _rewardDebt;
 
