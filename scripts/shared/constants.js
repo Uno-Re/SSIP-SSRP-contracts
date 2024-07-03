91f1e52c31ef8f582720db2818c8b501cd2c1f40
claudiovb <claudiocarvalhovilasboas@gmail.com>
2024-07-03 12:44:38 -0300

    WIP: Add UMA proper sepolia contract addresses | fix signature error

--- scripts/shared/constants.js
+++ scripts/shared/constants.js
@@ -26,6 +26,24 @@ const UNO_USDT_PRICE_FEED = {
   sepolia: "0x18Ca744fd1960d9Dda0Af5E22CC5C92aD75901E8",
 }
 
+//Helper link to UMA deployment contract addresses on sepolia: https://github.com/UMAprotocol/protocol/blob/master/packages/core/networks/11155111.json
+
+const OPTIMISM_ORACLE_V3 = {
+  sepolia: "0xFd9e2642a170aDD10F53Ee14a93FcF2F31924944",
+}
+
+const ADDRESS_WHITELIST_UMA = {
+  sepolia: "0xE8DE4bcE27f6214dcE18D8a7629f233C66A97B84",
+}
+
+const MOCK_ORACLE_ANCILLARY = {
+  sepolia: "0x5FE28AEa36420414692b1C907F7d0114d304eb0C",
+}
+
+const STORE_UMA = {
+  sepolia: "0x39e7FFA77A4ac4D34021C6BbE4C8778d47F684F2",
+}
+
 module.exports = {
   WETH_ADDRESS,
   UNISWAP_FACTORY_ADDRESS,
@@ -34,4 +52,8 @@ module.exports = {
   UNO,
   USDT,
   UNO_USDT_PRICE_FEED,
+  OPTIMISM_ORACLE_V3,
+  ADDRESS_WHITELIST_UMA,
+  MOCK_ORACLE_ANCILLARY,
+  STORE_UMA,
 }
